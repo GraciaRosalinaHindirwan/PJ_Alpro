65d7b02ca038a5bd35506ef529fe3bd570e18e1b
GraciaRosalinaHindirwan <rosalinagracia331@gmail.com>
2025-05-16 17:29:44 +0700

    create function input

--- pj_alpro/pj_alpro.cpp
+++ pj_alpro/pj_alpro.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <string.h>
 using namespace std;
 
 struct karyawan
@@ -6,11 +9,35 @@ struct karyawan
     char nama, jabatan, gaji;
     int NIP;
 };
-karyawan kry;
+karyawan kry[1000];
+
+
+void input(){
+    int data;
+    system("cls");
+    cout << " ========================" << endl;
+    cout << " | >> Input Karyawan >> |" << endl;
+    cout << " ========================" << endl;
+    cout << " Ingin Masukkan berapa data? "; cin >> data;
+    system("pause");
+    
+    for (int i = 0; i < data; i++)
+    {
+        cout << "Data ke-" << i+1 << endl;
+        cin.ignore();
+        cout << " Masukkan Nama    :     "; gets(kry[i].nama);
+        cout << " Masukkan NIP     :     "; gets(kry[i].NIP);
+        cout << " Masukkan Jabatan :     "; gets(kry[i].jabatan);
+        cout << " Masukkan Gaji    :     "; cin >> kry[i].gaji;
+        cin.ignore();
+        cout << " ========================" << endl;
+    }
+}
 
 int main(){
     int menu;
     char ulang = 'y';
+    system("cls");
     do
     {
         cout << " =================== " << endl;
@@ -21,6 +48,16 @@ int main(){
         cout << "3. EXIT              " << endl;
         cout << " =================== " << endl;
         cout << "Masukkan Menu : "; cin >> menu;
+
+        if (menu == 1)
+        {
+            input();
+        } if (menu == 2)
+        {
+            
+        }
+        
+        
     } while (ulang == 'y');
-    
+
 }
