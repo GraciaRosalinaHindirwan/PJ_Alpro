b08d8bc405e157b445ce9074cfa59897f15d4f94
GraciaRosalinaHindirwan <rosalinagracia331@gmail.com>
2025-05-16 16:29:44 +0700

    update menu

--- pj_alpro/pj_alpro.cpp
+++ pj_alpro/pj_alpro.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(){
     cout << "MENU: " << endl; 
     cout << "1. Input Karyawan " << endl;
-    cout << "2. Outpur Karyawan " << endl;
+    cout << "2. Output Karyawan " << endl;
+    cout << "exit" << endl;
+
     
 }
