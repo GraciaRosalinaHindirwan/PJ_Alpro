2b5096e3b2a7fb585288c50dc016a7b856645349
GraciaRosalinaHindirwan <rosalinagracia331@gmail.com>
2025-05-16 17:08:24 +0700

    update menu, create struct, adn build loop

--- pj_alpro/pj_alpro.cpp
+++ pj_alpro/pj_alpro.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 using namespace std;
 
+struct karyawan
+{
+    char nama, jabatan, gaji;
+    int NIP;
+};
+karyawan kry;
+
 int main(){
-    cout << "MENU: " << endl; 
-    cout << "1. Input Karyawan " << endl;
-    cout << "2. Output Karyawan " << endl;
-    cout << "exit" << endl;
-    cout << "exit2" << endl;
+    int menu;
+    char ulang = 'y';
+    do
+    {
+        cout << " =================== " << endl;
+        cout << "       MENU          " << endl;
+        cout << " =================== " << endl; 
+        cout << "1. Input Karyawan    " << endl;
+        cout << "2. Output Karyawan   " << endl;
+        cout << "3. EXIT              " << endl;
+        cout << " =================== " << endl;
+        cout << "Masukkan Menu : "; cin >> menu;
+    } while (ulang == 'y');
+    
 }
